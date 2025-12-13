a06d02c86b7f9c448ffca0edf3776c796977489d
monjiapawne <iambrendamore@gmail.com>
2025-12-12 21:15:47 -0500

    add: markdownr render

--- src/github_walker/display.py
+++ src/github_walker/display.py
@@ -1,7 +1,8 @@
 import textwrap
 from github.Repository import Repository
+import markdown
 
-def display(repos: list[Repository]):
+def display_cli(repos: list[Repository]):
     for i, repo in enumerate(repos, 1):
         print(f"{i})")
         print(f"Name: {repo.name}")
@@ -10,3 +11,19 @@ def display(repos: list[Repository]):
         print(f"Desc: {'\n'.join(textwrap.wrap(repo.description, 70))}")
         print(f"Url: {repo.html_url}")
         print("\033[32m", "─" * 70, "\033[0m")
+
+
+def display(repos: list[Repository]):
+    lines = ["# Github Curator\n\n"]
+
+    for repo in repos:
+        lines.append(f"## {repo.name}\n\n")
+        lines.append(f"> {repo.description}  \n\n")
+        lines.append(f"Stars: {repo.stargazers_count}  \n")
+        lines.append(f"Issues: {repo.open_issues}  \n")
+        lines.append(f"{repo.html_url}  \n\n")
+
+    text = "".join(lines)
+
+    with open("README.md", 'w') as f:
+        f.write(text)
